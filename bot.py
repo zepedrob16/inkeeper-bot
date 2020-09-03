9a65b94a3de110bcaf3a512243388577145ef373
José Pedro Borges <up201503603@fe.up.pt>
2020-09-03 21:52:17 +0100

    started adding metacritic support

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import os
 import random
 import discord
+import requests
+import json
 from dotenv import load_dotenv
 from howlongtobeatpy import HowLongToBeat
 from discord.ext import commands
@@ -11,9 +13,14 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='ttb', help='Responds with a random quote from Brooklyn 99')
-async def nine_nine(ctx, *game_full_name):
+metacritic_url = "https://chicken-coop.p.rapidapi.com/games"
+headers = {
+    'x-rapidapi-host': "chicken-coop.p.rapidapi.com",
+    'x-rapidapi-key': "e1d46b5a6cmsh435ae7b9b898362p193c22jsn03fc6e995827"
+}
 
+@bot.command(name='ttb', help='Responds with information obtained from howlongtobeat')
+async def howlongtobeat(ctx, *game_full_name):
     game = ''
     
     if len(game_full_name) > 1:
@@ -40,14 +47,29 @@ async def nine_nine(ctx, *game_full_name):
     embed.add_field(name = "Main + Extra", value = best_element.gameplay_main_extra + "hours", inline = True)
     embed.add_field(name = "Completionist", value = best_element.gameplay_completionist + "hours", inline = True)
 
-   # response = (
-    #        f'**Main Story**: {best_element.gameplay_main} hours \n'
-    #        f'**Main + Extra**: {best_element.gameplay_main_extra} hours \n'
-   #         f'**Completionist**: {best_element.gameplay_completionist} hours \n'
-   #     )
-
-    #await message.channel.send(embed=embed)
-
     await ctx.send(embed=embed)
+
+@bot.command(name='meta', help='Responds with information obtained from metacritic')
+async def metacritic(ctx, *game_full_name):
+    game = ''
+    
+    if len(game_full_name) > 1:
+        for element in game_full_name:
+            game = game + element + ' '
+    
+    else:
+        game = game_full_name[0]
+
+    querystring = {"title": game}
+
+    response = requests.request("GET", metacritic_url, headers=headers, params=querystring)
+
+    results = json.loads(response.text)["result"]
+    
+    if results is not None and len(results) > 0:
+        best_element = results[0]
+
+    print(best_element)
+
 
 bot.run(TOKEN)
